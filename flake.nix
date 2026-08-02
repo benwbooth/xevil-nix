210ef767fa9a69abef55017796fc373c1396d9ac
Ben Booth <benwbooth@gmail.com>
2026-08-02 11:21:55 -0700

    Keep flake inputs alive in dev shells

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = inputs@{ self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -134,6 +134,12 @@
         };
 
         devShells.default = pkgs.mkShell {
+          FLAKE_INPUTS = builtins.concatStringsSep ":" (
+            map (input: input.outPath) (
+              builtins.attrValues (builtins.removeAttrs inputs [ "self" ])
+            )
+          );
+
           inputsFrom = [ xevil ];
         };
       }
